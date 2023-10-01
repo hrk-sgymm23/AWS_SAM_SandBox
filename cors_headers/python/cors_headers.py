883b1af0b047445550a9a474d4c83470e699066a
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2023-10-01 15:17:26 +0900

    chore: layer削除

--- cors_headers/python/cors_headers.py
+++
@@ -1,6 +0,0 @@
-def get_cors_headers(origin):
-    return {
-        'Access-Control-Allow-Origin': origin,
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-    }
